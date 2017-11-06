81b6cb7edc7d418b4649fcfcc928941edc4a76fb
Gowdham <gowdhaman@czsm.co.in>
2017-11-06 19:35:54 +0530

    Comited by siva.,Worked on Profile screen,Settings screen,Main storyboard, Profile_post screen

---
+++ Numnu/FoodPreferenceTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FoodPreferenceTableViewCell.swift
+//  Numnu
+//
+//  Created by Gowdhaman on 06/11/17.
+//  Copyright © 2017 czsm. All rights reserved.
+//
+
+import UIKit
+
+class FoodPreferenceTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Numnu/SettingsViewController.swift
+++ Numnu/SettingsViewController.swift
@@ -95,14 +95,11 @@ class SettingsViewController: UIViewController,UITableViewDelegate,UITableViewDa
     
     @IBAction func didTappedEdit(_ sender: Any) {
         
-        
-//        let SettingsViewController = self.storyboard?.instantiateViewController(withIdentifier: "profileid") as! SettingsViewController
-//        self.navigationController?.pushViewController(SettingsViewController, animated: true)
-        let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
-        
+        let storyBoard : UIStoryboard = UIStoryboard(name: Constants.Main, bundle:nil)
         let editProfile = storyBoard.instantiateViewController(withIdentifier: "profileid") as! Edit_ProfileVC
 //        self.present(editProfile, animated:true, completion:nil)
         self.navigationController?.pushViewController(editProfile, animated: true)
+        
     }
     /*
     // MARK: - Navigation
