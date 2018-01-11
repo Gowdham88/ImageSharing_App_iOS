d017468a9ea790d001f479deb860ab3b6c8ef2c5
Gowdham <gowdhaman@czsm.co.in>
2018-01-11 13:28:01 +0530

    Event storyboard appmap implemented

--- Numnu/EventViewController.swift
+++ Numnu/EventViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import XLPagerTabStrip
 import Alamofire
 import SwiftyJSON
-
+import MapKit
 import Nuke
 import NVActivityIndicatorView
 
@@ -294,7 +294,8 @@ extension EventViewController {
     
     func mapRedirect(sender:UITapGestureRecognizer) {
         
-        openMapBoard()
+//        openMapBoard()
+          openApplemap()
     }
     
     /*************************Tag view updating************************************/
@@ -392,6 +393,26 @@ extension EventViewController {
         self.navigationController!.pushViewController(vc, animated: true)
         
     }
+    
+    func openApplemap(){
+       
+        let lat:CLLocationDegrees = 41.887
+        let log:CLLocationDegrees = -87.662
+
+        let regionDistance:CLLocationDistance = 1000;
+        let coordinates = CLLocationCoordinate2DMake(lat, log)
+        let regionSpan = MKCoordinateRegionMakeWithDistance(coordinates, regionDistance, regionDistance)
+
+
+        let options = [MKLaunchOptionsMapCenterKey: NSValue(mkCoordinate: regionSpan.center), MKLaunchOptionsMapSpanKey: NSValue(mkCoordinateSpan: regionSpan.span)]
+
+        let placemark = MKPlacemark(coordinate: coordinates)
+        let mapItem = MKMapItem(placemark: placemark)
+        mapItem.openInMaps(launchOptions: options)
+        print(lat)
+        print(log)
+        
+    }//openApplemap
     
     func alertTapRegister() {
         
